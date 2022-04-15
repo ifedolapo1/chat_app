1755e70a6f218659541f587aea66f85e5358da35
Ifedolapo Folakemi Akanni <Ifedolapoakanni@gmail.com>
2022-04-15 03:41:02 +0100

    socket connection to server with eventlet

--- server_app.py
+++ server_app.py
@@ -1,58 +1,62 @@
-from twisted.internet import reactor
-from twisted.internet import protocol
-from kivy.uix.label import Label
-from kivy.app import App
-from kivy.support import install_twisted_reactor
 import sqlite3
-# Uncomment below line if you don't have twisted reactor installed
-# install_twisted_reactor()
+import socketio
+import eventlet
+import sqlite3  # Import from python library
+from database import con, cur, runSqlDatabase 
+import hashlib # Hashing Method from Python library
+from operator import itemgetter
+
+sio = socketio.Server() # Create a web socket server
+app = socketio.WSGIApp(sio) # Wrap socket in a WSGI application
+
+@sio.event
+def connect(sid, environ):
+    # sio.save_session(sid, {'username': None})
+    print('connect ', sid)
+
+@sio.on('message')
+def my_message(event, sid, data):
+    print('message ', data)
+
+@sio.on('auth', namespace='/login')
+def login(sid, data):
+    print('sid', sid)
+    username, password = itemgetter('username', 'password')(data)
+    # Secure user password using md5 hash method
+    secure_password = hashlib.md5(password.encode()).hexdigest()
+    print(username, " - ", password)
+    user = cur.execute("SELECT id, username FROM users WHERE username = '%s' AND password = '%s'" % (username, secure_password)).fetchone()
+    con.commit()
+    print(user)
+    if user is None:
+        return {
+            'status': 'error',
+            'id': None
+        }
+    user_data = {
+        'status': 'success',
+        'id': str(user[0]),
+        'username': str(user[1])
+    }
+    with sio.session(sid) as session:
+        session['username'] = str(user[1])
+        session['id'] = str(user[0])
+    return user_data
+
+@sio.on('chat', namespace='/chat')
+def chat(sid, data):
+    print('chat', data)
+    pass
+
+@sio.event
+def on_disconnect(sid):
+    print('disconnect ', sid)
 
 
-class EchoServer(protocol.Protocol):
-    def dataReceived(self, data):
-        print(data)
-        response = self.factory.app.handle_message(data)
-        if response:
-            self.transport.write(response)
-
-
-class EchoServerFactory(protocol.Factory):
-    protocol = EchoServer
-
-    def __init__(self, app):
-        self.app = app
-
-
-class TwistedServerApp(App):
-    label = None
-
-    def build(self):
-        self.sql_build()
-        self.label = Label(text='server started\n')
-        reactor.listenTCP(8000, EchoServerFactory(self))
-        return self.label
-
-    def sql_build(self):
-        con = sqlite3.connect("database.db")
-        cur = con.cursor()
-        # Check if table exists in database before creating
-        cur.execute(
-            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username, password)")
-        cur.execute(
-            "CREATE TABLE IF NOT EXISTS chats (id INTEGER PRIMARY KEY, from_username, to_username, text,  enc_text, time)")
-
-        con.commit()
-        con.close()
-
-    def handle_message(self, msg):
-        msg = msg.decode('utf-8')
-        self.label.text = "received:  {}\n".format(msg)
-
-        if msg == "send":
-            msg = "received"
-        self.label.text += "responded: {}\n".format(msg)
-        return msg.encode('utf-8')
-
+class ChatServerApp():
+    def __init__(self) -> None:
+        runSqlDatabase()
+        eventlet.wsgi.server(eventlet.listen(('', 5000)),app)
 
 if __name__ == '__main__':
-    TwistedServerApp().run()
+    ChatServerApp()
