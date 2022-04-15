becc9ac27a9eed2cc5998d5aa3f0c4dd707b1f41
Ifedolapo Folakemi Akanni <Ifedolapoakanni@gmail.com>
2022-04-15 03:41:15 +0100

    seperate db logic

--- database.py
+++ database.py
@@ -1,37 +1,16 @@
-import sqlite3
-from sqlite3 import Error
-import datetime
+import sqlite3  # Import from python library
 
+db_config_file = 'database.db'
 
-class Database:
-    def __init__(self) -> None:
-        self.db = sqlite3.connect('database.db')
-        self.cur = self.db.cursor()
+# SQLite database connection
+con = sqlite3.connect(db_config_file)
+cur = con.cursor()
 
-    def sql_query(self, sql, *params):
-        self.cur.execute(sql, params)
-        self.cur.commit()
-
-    def insert(self, row):
-        self.cur.execute('insert into {} (t1, i1) values (?, ?)'.format(
-            self._table), (row['t1'], row['i1']))
-        self.cur.commit()
-
-    def retrieve(self, key):
-        cursor = self._db.execute(
-            'select * from {} where t1 = ?'.format(self._table), (key,))
-        return dict(cursor.fetchone())
-
-    def update(self, row):
-        self._db.execute(
-            'update {} set i1 = ? where t1 = ?'.format(self._table),
-            (row['i1'], row['t1']))
-        self._db.commit()
-
-    def delete(self, key):
-        self._db.execute(
-            'delete from {} where t1 = ?'.format(self._table), (key,))
-        self._db.commit()
-
-    def __del__(self):
-        self.db.close()
+def runSqlDatabase():
+    # Create users and chats table if it does not exists
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username, password)")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS chats (id INTEGER PRIMARY KEY, from_username, to_username, text,  enc_text, time)")
+    con.commit() # Save database execution changes
+    # con.close() # Close database connection
